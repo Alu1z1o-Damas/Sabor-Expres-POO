3c2662a3a0468506a7f03f86c49e937e61acb206
Aluizio Damas <135044419+Alu1z1o-Damas@users.noreply.github.com>
2026-08-12 12:58:43 -0300

    aula02 atributos

--- modelos/restalrante.py
+++ modelos/restalrante.py
@@ -1,14 +1,13 @@
 class Restaurantes:
-    nome = ''
-    categoria = ''
-    ativo = False
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = nome
+        self.ativo = False
 
-restaurante_praca = Restaurantes()
-restaurante_praca.nome = 'Praça'
-restaurante_praca.categoria = 'Gourmet'
-
-restaurante_pizza = Restaurantes()
+restaurante_praca = Restaurantes('Praça', 'Gourmet')
+restaurante_pizza = Restaurantes('Pizza Express', 'Italiano')
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
 print(vars(restaurante_praca))
+print(vars(restaurante_pizza))
